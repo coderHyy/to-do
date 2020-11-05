99abf0e9703b4865f4387d27cbc6a5723c4f9289
Hyy <1014873097@qq.com>
2020-11-05 19:04:44 +0800

    init

---
+++ src/views/todolist/Todolist.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="list">
+		<input v-model="content" />
+		<button @click="save">提交</button>
+		<list :model="model"></list>
+	</div>
+</template>
+
+<script>
+	import list from './list.vue'
+	export default {
+		name: "Todolist",
+		components: {
+			list
+		},
+		data() {
+			return {
+				content: '',
+				model: [{
+						title: '1',
+						list: ['今日工作', 'gogogo']
+					},
+					{
+						title: '2',
+						list: ['123', '456']
+					}
+				]
+			}
+		},
+		methods: {
+			save() {
+				this.model[0].list.unshift(this.content)
+				// console.log(this.model)
+			}
+		}
+	};
+</script>
---
+++ src/views/todolist/list.vue
@@ -0,0 +1,62 @@
+<template>
+	<div class="list">
+		<div v-for="(item,i) in model" :key="i">
+			<h3>{{item.title | showTitle}}</h3>
+			<ul>
+				<li v-for="(list,index) in item.list" :key="index">
+					<div class="item">
+						<div style="flex: 1;">{{list}}</div>
+						<div style="padding-right: 1rem;">完成</div>
+						<div @click="del(item.list,index)">删除</div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		props: {
+			model: {
+				type: Array,
+				default () {
+					return []
+				}
+			}
+		},
+		methods: {
+			del(list, index) {
+				console.log(list, index)
+				list.splice(index, 1)
+			}
+		},
+		filters: {
+			showTitle(value) {
+				if (value == 1) {
+					return '待办'
+				} else {
+					return '已完成'
+				}
+			}
+		}
+	}
+</script>
+
+<style scoped lang="scss">
+	.list {
+		ul {
+			padding: 0;
+
+			li {
+				list-style: none;
+			}
+		}
+
+		.item {
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+		}
+	}
+</style>
